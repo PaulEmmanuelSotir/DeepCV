d727f8fb36a92736b5977909d7a1bef970c221ef
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2020-05-13 10:01:36 +0200

    Implemented basic principles Deep Feature Flow for Video Recognition 2017 Paper in 'deepcv.video'
    + reshaped some deepcv subpackages (removed 'deepcv.tracking', created
    'deepcv.video' which now contains 'optical_flow', 'tracking' and
    'deep_feature_flow' modules
    + modified '__init__.py' files

--- src/deepcv/meta/__init__.py
+++ src/deepcv/meta/__init__.py
@@ -6,17 +6,17 @@ __all__ = ['code', 'data', 'uncertainty', 'active_learning', 'base_module', 'com
 
 # 'deepcv.meta' modules imports
 # import deepcv.meta.active_learning
-# import deepcv.meta.base_module
+import deepcv.meta.base_module
 # import deepcv.meta.compression
 # import deepcv.meta.contrastive
 # import deepcv.meta.debug
 # import deepcv.meta.distributed
 # import deepcv.meta.embeddings
 # import deepcv.meta.generative
-# import deepcv.meta.hyperparams
-# import deepcv.meta.ignite_training
+import deepcv.meta.hyperparams
+import deepcv.meta.ignite_training
 # import deepcv.meta.multitask
-# import deepcv.meta.nn
+import deepcv.meta.nn
 # import deepcv.meta.one_cycle
 # import deepcv.meta.portability
 # import deepcv.meta.regularization
@@ -24,4 +24,4 @@ __all__ = ['code', 'data', 'uncertainty', 'active_learning', 'base_module', 'com
 # import deepcv.meta.stackensembling
 
 # default 'deepcv.meta' subpackages imports
-#import deepcv.meta.data
+import deepcv.meta.data
---
+++ src/deepcv/video/deep_feature_flow.py
@@ -0,0 +1,39 @@
+
+from typing import Tuple, Sequence
+
+import torch
+from torch import nn
+
+import deepcv.utils
+
+def deep_feature_flow_inference(video_frames: Iterable[torch.Tensor], features_model: nn.Module, tasks_models: Sequence[nn.Module], flow_estimation: nn.Module, keyframe_period: int) -> Generator[Dict[int, Tuple], None, None]:
+    """ Implementation of [Deep Feature Flow for Video Recognition 2017 Paper](https://arxiv.org/abs/1611.07715)
+    Args:
+        - video_frames: Generator or iterable which yields input video frames
+        - features_model:
+        - tasks_models:
+        - keyframe_period: Frame period at which model is performing inference without propagation. Inference on frames in between which are not key-frames will be performed on propagated features using optical flow (avoids applying `features_model` for each video frames). """
+    assert keyframe_period > 1, f'Error: Keyframe period must be greater than 1: `keyframe_period={keyframe_period}`.'
+    feature_maps = features_model(video_frames.__next__())
+    yield {0: (task_network(feature_maps) for task_network in tasks_models)}
+    
+    def _scale(current_frame: torch.Tensor, key_frame: torch.Tensor) -> torch.Tensor:
+        return ... # TODO: implement _scale function
+    
+    def _propagation_W(keyframe_features: torch.Tensor, 2D_flow_field: torch.Tensor, scale_field: torch.Tensor) -> torch.Tensor:
+        return ... # TODO: implement _propagation_W function
+
+    for i, frame in enumerate(video_frames):
+        if (i + 1) % keyframe_period == 0:
+            feature_maps = features_model(frame)
+            keyframe, keyframe_features = frame, feature_maps
+        else:
+            # Propagation using optical flow and previous key frame results
+            scale_field = _scale(frame, keyframe) 
+            2D_flow_field = flow_estimation(frame, keyframe)
+            feature_maps = _propagation_W(keyframe_features, 2D_flow_field, scale_field) 
+        yield {i + 1: (task_network(feature_maps) for task_network in tasks_models)}
+
+if __name__ == '__main__':
+    cli = deepcv.utils.import_tests().test_module_cli(__file__)
+    cli()
